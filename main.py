9dceaef1d5a55afb0b38ad1111a281ad2c652c90
Renz <alightmotionprem229@gmail.com>
2024-12-30 11:21:26 +0700

    up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import uvloop
+from .core.bot import Bot
 
-if __name__ == "__main__":
-    import uvloop
-    from core.bot import Bot
-    uvloop.install()
-    Bot().run()
+uvloop.install()
+Bot().run()
