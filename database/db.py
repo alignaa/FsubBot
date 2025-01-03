544af96299ee1924a3fbd443fdde7a68ad7a2fb7
Renz <alightmotionprem229@gmail.com>
2025-01-03 19:46:05 +0700

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,72 +1,32 @@
-import threading
-from sqlalchemy import TEXT, Column, Numeric, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from pymongo import MongoClient
-from config import DATABASE_TYPE, DB_URL, DB_NAME
+import sqlite3
+from threading import RLock
 
+DB_PATH = "database.db"
+conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+cursor = conn.cursor()
 
-# Database SQL
-if DATABASE_TYPE == 'sql':
-    def start() -> scoped_session:
-        engine = create_engine(DB_URL, client_encoding="utf8")
-        BASE.metadata.bind = engine
-        BASE.metadata.create_all(engine)
-        return scoped_session(sessionmaker(bind=engine, autoflush=False))
-    BASE = declarative_base()
-    SESSION = start()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS broadcast (
+        bot_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        user_name TEXT
+    )
+''')
+conn.commit()
 
-    INSERTION_LOCK = threading.RLock()
+INSERTION_LOCK = RLock()
 
-    class Broadcast(BASE):
-        __tablename__ = "broadcast"
-        id = Column(Numeric, primary_key=True)
-        user_name = Column(TEXT)
-
-        def __init__(self, id, user_name):
-            self.id = id
-            self.user_name = user_name
-
-    Broadcast.__table__.create(checkfirst=True)
-
-
-# Database mongo
-elif DATABASE_TYPE == 'mongo':
-    dbclient = MongoClient(DB_URL)
-    database = dbclient[DB_NAME]
-    user_data = database['users']
-
-# Database MongoDB dan SQL
-def add_user(id, user_name):
-    if DATABASE_TYPE == 'sql':
-        with INSERTION_LOCK:
-            msg = SESSION.query(Broadcast).get(id)
-            if not msg:
-                usr = Broadcast(id, user_name)
-                SESSION.add(usr)
-                SESSION.commit()
-    elif DATABASE_TYPE == 'mongo':
-        found = user_data.find_one({'_id': id})
-        if not found:
-            user_data.insert_one({'_id': id, 'user_name': user_name})
+def add_user(bot_id, user_id, user_name):
+    with INSERTION_LOCK:
+        cursor.execute("INSERT INTO broadcast (bot_id, user_id, user_name) VALUES (?, ?, ?)", (bot_id, user_id, user_name))
+        conn.commit()
 
 def full_userbase():
-    if DATABASE_TYPE == 'sql':
-        users = SESSION.query(Broadcast).all()
-        SESSION.close()
-        return [int(user.id) for user in users]
-    elif DATABASE_TYPE == 'mongo':
-        user_docs = user_data.find()
-        user_ids = [doc['_id'] for doc in user_docs]
-        return user_ids
+    cursor.execute("SELECT bot_id, user_id, user_name FROM broadcast")
+    rows = cursor.fetchall()
+    return rows
 
-
-def del_user(user_id: int):
-    if DATABASE_TYPE == 'sql':
-        with INSERTION_LOCK:
-            user_to_delete = SESSION.query(Broadcast).filter_by(id=user_id).first()
-            if user_to_delete:
-                SESSION.delete(user_to_delete)
-                SESSION.commit()
-    elif DATABASE_TYPE == 'mongo':
-        user_data.delete_one({'_id': user_id})
+def del_user(bot_id):
+    with INSERTION_LOCK:
+        cursor.execute("DELETE FROM broadcast WHERE bot_id = ?", (bot_id,))
+        conn.commit()
